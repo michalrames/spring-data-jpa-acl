6638cc3ee3c56e0d03beb964f8f1d0d0bd4fb9fd
Mohit Yadav <mohit@mediaiqdidital.com>
2015-01-19 17:48:36 +0530

    removed spring boot dependency

--- src/test/java/com/mediaiqdigital/spring/acl/jpa/ApplicationTests.java
+++ src/test/java/com/mediaiqdigital/spring/acl/jpa/ApplicationTests.java
@@ -2,11 +2,11 @@ package com.mediaiqdigital.spring.acl.jpa;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(locations = "classpath:application-context.xml")
+@ContextConfiguration(locations = "classpath:application-context.xml")
 public class ApplicationTests {
 
 	@Test
