cd4b6d6678635007c56b74e463dc8b5363bc91e3
Michal Rames <michal.rames@gmail.com>
2018-07-07 20:52:28 +1000

    added foregin key descriptors

--- src/main/java/com/mediaiqdigital/spring/acl/jpa/domain/AclObjectIdentity.java
+++ src/main/java/com/mediaiqdigital/spring/acl/jpa/domain/AclObjectIdentity.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import javax.persistence.ForeignKey;
 
 import org.hibernate.annotations.Immutable;
 
@@ -36,18 +37,18 @@ public class AclObjectIdentity implements Serializable {
 	private Long objectIdIdentity;
 
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "object_id_class", referencedColumnName = "id", nullable = false, unique = false, insertable = true, updatable = true)
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_acl_object_identity_class"), name = "object_id_class", referencedColumnName = "id", nullable = false, unique = false, insertable = true, updatable = true)
 	private AclClass objectIdClass;
 
 	@Column(name = "entries_inheriting", nullable = false, unique = false)
 	private Boolean entriesInheriting;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "parent_object", referencedColumnName = "id", nullable = true, unique = false, insertable = true, updatable = true)
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_acl_object_identity_parent"), name = "parent_object", referencedColumnName = "id", nullable = true, unique = false, insertable = true, updatable = true)
 	private AclObjectIdentity parentObject;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "owner_sid", referencedColumnName = "id", nullable = true, unique = false, insertable = true, updatable = true)
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_acl_object_identity_owner"), name = "owner_sid", referencedColumnName = "id", nullable = true, unique = false, insertable = true, updatable = true)
 	private AclSid ownerSid;
 
 	@OneToMany(targetEntity = com.mediaiqdigital.spring.acl.jpa.domain.AclEntry.class, fetch = FetchType.LAZY, mappedBy = "aclObjectIdentity", cascade = CascadeType.REMOVE)
